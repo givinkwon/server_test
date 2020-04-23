3d23b75a4ed93bff5d015b68874c53f69c45acf7
Nut Bolt & <boltnnut@boltnnut.com>
2020-04-23 17:21:31 +0900

    20200423

--- api/board/serializers.py
+++ api/board/serializers.py
@@ -5,9 +5,9 @@ from apps.board.models import *
 class NoticeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
-        fields = ['title', 'content', 'is_top', 'created_at']
+        fields = ['id', 'title', 'content', 'is_top', 'created_at']
 
 class MagazineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Magazine
-        fields = ['title', 'image', 'is_top', 'created_at', 'link']
+        fields = ['id', 'title', 'image', 'is_top', 'created_at', 'link']
