33ee48a88b774feccba8d42b3248c70a5b7417c3
Nut Bolt & <boltnnut@boltnnut.com>
2020-04-24 18:55:50 +0900

    20200424

--- api/account/viewsets.py
+++ api/account/viewsets.py
@@ -545,9 +545,9 @@ class PartnerViewSet(viewsets.ModelViewSet):
 
 
     @swagger_auto_schema(request_body=PartnerSerializer)
-    @action(detail=False, methods=('POST',), url_path='request-partner', http_method_names=('post',),)
+    @action(detail=False, methods=('GET',), url_path='request-partner', http_method_names=('get',),)
     def find_partner(self, request, *args, **kwargs):  # 의뢰서 완성 시에 적합한 파트너 리스트 추천
-        subclass = request.data.get('subclass')
+        subclass = request.GET('subclass')
         #partner_qs
        # print(subclass)
         partner1_qs = Partner.objects.filter(possible_set__id = subclass)
