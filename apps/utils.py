8e82b938583b17eded46d78d711ac4188310b617
Nut Bolt & <boltnnut@boltnnut.com>
2020-04-25 00:27:35 +0900

    20200425

--- apps/utils.py
+++ apps/utils.py
@@ -92,7 +92,6 @@ class kakaotalk(object):
                              }
                          ]}}}
              headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-             response = 0
              response = requests.post(url, data=json.dumps(data), headers=headers)
             return response
 
@@ -107,7 +106,7 @@ class kakaotalk_request(object):
              data = {'account': 'boltnnut_korea', 'refkey': 'bolt123', 'type': 'at', 'from': '01028741248',
                      'to': phone, 'content': {
                    'at': {'senderkey': '44e4fdc989b12906c82fc46e428dd91dd99f0d98', 'templatecode': 'answer_to_client',
-                            'message': '고객님의 의뢰에 대한 전문가의 제안서가 도착하였습니다.',
+                            'message': '고객님의 의뢰에 대한 전문가의 제안서가 도착하였습니다.'+'\n'+'* 해당 메세지는 고객님께서 요청하신 의뢰에 대한 제안이 있을 경우 발송됩니다',
 
                           'button': [
                                 {
@@ -118,6 +117,5 @@ class kakaotalk_request(object):
                              }
                          ]}}}
              headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-             response = 0
              response = requests.post(url, data=json.dumps(data), headers=headers)
             return response
